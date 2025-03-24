9e23973c108bee14b9cd64c7fd6c90fc4c82a3b3
cyrillans <cyrillanathania@students.usu.ac.id>
2025-03-24 09:50:58 +0700

    Pertemuan 4 : Perulangan

---
+++ loop.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int  main() 
+{
+//     // Goto Label
+//     // Hello world, Fasilkom-TI, Ilmu Komputer, IKLC
+//     // -> Hello world, IKLC, Ilmu Komputer, Fasilkom-TI
+
+//     a : 
+//         cout << "Hello World" << endl;
+//         goto d;
+//     b : 
+//         cout << "Fasilkom-TI" << endl;
+//         return 0;
+//     c : 
+//         cout << "Ilmu Komputer" << endl;
+//         goto b;
+//     d : 
+//         cout << "IKLC" << endl;
+//         // goto c;
+// 
+    //menampilkan bilangan genap 10 -> dengan goto
+    // int i = 10;
+    // genap :
+    // if (i % 2 == 0) {
+    //     cout << i << endl;
+    // } i--;
+
+    //     if (i >= 2) {
+    //         goto genap;
+    //     }
+
+    // Statement while
+    // int i = 1;
+    // while (i >= 10) {
+    //     if (i % 2 == 0) {
+    //         cout << i << " ";
+    //     } i++;
+    // }
+
+    // statement do-while -> perulangan minimal berjalan sekali, meskipun false
+    // int i = 1;
+    // do {
+    //     cout << i << endl;
+    // } while (1 <= 0);
+
+    // Statement for
+    // for (inisialisai, kondisi, increase)
+   
+    //Nested for
+    // ***** 5 x 5
+    // for (int i = 1; i <= 5; i++) { // untuk baris
+    //     for (int j = 1; j<= 5; j++) { // umtuk kolom
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    //}
+
+    //Segitiga siku-siku
+    // for (int i = 1; i <= 5; i+=) {
+    //     for (j = 1; j <= i; j++) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+}
