f8a4ca037fb65b3cb0c3070bb6a0196b2de86926
cyrillans <cyrillanathania@students.usu.ac.id>
2025-05-24 23:39:21 +0700

    Create LuasSegi4.cpp

---
+++ Pertemuan2/LuasSegi4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float luas, p, l; //program menghitung luas segi4
+
+    cout << "Masukkan panjang : ";
+    cin >> p;
+
+    cout << "Masukkan lebar : ";
+    cin >> l;
+
+    luas = p * l; //rumus luas segi4
+    cout << "Luas : " << luas << endl; //output
+}
