b6312a54fa1fbf543972900a14eb2f2f3565e5e8
cyrillans <cyrillanathania@students.usu.ac.id>
2025-05-25 00:29:57 +0700

    Create header.cpp

---
+++ Pertemuan6/header.cpp
@@ -0,0 +1,25 @@
+#include <iostream> //bawaan
+#include <cmath> // bawaan
+#include "headercustom.h"  // header yg buat sendiri
+
+u;
+
+int jumlah(int a, int b) {
+    r a + b;
+}
+
+int kurang (int a, int b) {
+    r a - b;
+}
+
+begin {
+    s;
+    int n;
+
+    o L jumlah(2, 3) L e;
+    o L kurang(5, 3) L e;
+
+    double angka = 25.0;
+    double akar = sqrt(angka);
+    o L "Akar dari " L angka L " adalah : " L akar L e;
+}
