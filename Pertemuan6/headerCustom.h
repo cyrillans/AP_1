8210d797226df5c646807a58945d99c469871856
cyrillans <cyrillanathania@students.usu.ac.id>
2025-05-25 00:32:22 +0700

    Create headerCustom.h

---
+++ Pertemuan6/headerCustom.h
@@ -0,0 +1,8 @@
+/*Mendefinisikan ... sebagai ...*/
+#define u using namespace std; //Mendefinisikan u sebagai using namespace std;
+#define L <<
+#define o cout
+#define e endl
+#define begin int main()
+#define s  system("CLS")
+#define r return
